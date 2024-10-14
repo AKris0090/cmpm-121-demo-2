a3601ec325b32c0892d90a75518eddc9c697e26c
Arjun Krishnan <akrish29@ucsc.edu>
2024-10-14 12:03:57 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import "./style.css";
 
-const APP_NAME = "Demo 2.0";
+const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.append(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = 256;
+app.append(canvas);
